f978e4fd37657be9f325af1545e8b7babebe5a1f
Manuel Limones <limonesu.me@gmail.com>
2015-05-10 01:24:33 -0500

    Add suport for keil IDE (still using arm-none-eabi)

---
+++ mdk/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SPL_04_UART' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
